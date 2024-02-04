0c98fa56c1da194640470ff61b3738386f8bb3e3
Magomed Alimkhanov <scrythepvp@gmail.com>
2024-02-04 20:05:24 +0100

    feat: switch file tree and shortcuts and alpha
    switch file tree from neo-tree to nvim-tree
    also add some shortcuts for moving around faster
    in insert mode and normal around windows (jkhl)
    also add alpha for splash screen

--- dotfiles/nvim/.config/nvim/lua/config.lua
+++ dotfiles/nvim/.config/nvim/lua/config.lua
@@ -14,3 +14,13 @@ vim.opt.expandtab = true
 vim.opt.colorcolumn = "80"
 
 vim.cmd("set clipboard+=unnamedplus")
+
+vim.keymap.set("i", "<C-h>", "<Left>")
+vim.keymap.set("i", "<C-l>", "<Right>")
+vim.keymap.set("i", "<C-j>", "<Down>")
+vim.keymap.set("i", "<C-k>", "<Up>")
+
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
---
+++ dotfiles/nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,14 @@
+return {
+	"nvim-tree/nvim-tree.lua",
+	version = "*",
+	lazy = false,
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		require("nvim-tree").setup({
+			view = { relativenumber = true },
+		})
+		vim.keymap.set("n", "<C-n>", "<cmd>:NvimTreeToggle<CR>")
+	end,
+}
