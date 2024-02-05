e06bf3fa026fd6cac1bcd5aad2f32340af79112e
Magomed Alimkhanov <scrythepvp@gmail.com>
2024-02-05 23:47:58 +0100

    feat: add fugitive and gitsigns

---
+++ dotfiles/nvim/.config/nvim/lua/plugins/git.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup({
+        current_line_blame = true,
+        current_line_blame_opts = {
+          delay = 0,
+        },
+      })
+      vim.keymap.set("n", "gp", "<cmd>Gitsigns preview_hunk_inline<CR>", {})
+    end,
+  },
+  {
+    "tpope/vim-fugitive",
+    config = function()
+      vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+    end,
+  },
+}
